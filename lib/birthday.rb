ad470bbaea85850bff276c8bbce7fc25ad6796fb
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-16 23:46:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy #{age} birthday, #{name}"
+    puts "Happy birthday #{name}! You are now #{age} years old"
   end
 end
 
